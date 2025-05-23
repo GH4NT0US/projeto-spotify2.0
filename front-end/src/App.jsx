fd8949f43eaf2d51a25eb56e1a2362b17d714f47
GH4NT0US <juliancaires@hotmail.com>
2025-05-23 10:50:44 -0300

    Corrigindo importação da Home

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from './pages/home';
+import Home from './pages/Home';
 import Artists from './pages/artists';
 import Artist from './pages/Artist';
 import Songs from './pages/Songs';
